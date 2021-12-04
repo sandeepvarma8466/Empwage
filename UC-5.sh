5b4c7ca0f490f0ff9160670e44bd0f289b87600f
sandeepvarma8466 <svarma852@gmail.com>
2021-12-04 20:28:32 +0530

    added UC-5

---
+++ UC-5.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+isPartTime=1
+isFullTIme=2
+totalSalary=0
+empRatePerHr=20
+numOfWorkingDays=20
+
+for (( day-1; day<=$numOfWorkingDays; day++))
+do
+	empCheck=$((RANDOM%3))
+		case $empCHeck in
+				$isPartTime)
+					empHrs=4
+				;;
+				$isFullTime)
+					empHrs=8
+				;;
+				*)
+				empHrs=0
+				;;
+		esac
+	salary=$(($empHrs*$empRatePerHr))
+	totalSalary=$(($salary+$totalSalary))
+done
